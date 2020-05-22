753c5a1b55b301d6d3fa5cf95f90dd4636d29f52
krishnam <saikrishnamraju384@gmail.com>
2020-05-22 12:00:35 +0530

    added items to cart when clicked

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,8 @@
 import cartActionTypes from './cart.types.js';
+import {addItemToCart} from './cart.utils';
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const CartReducer = (state = INITIAL_STATE, action ) => {
@@ -10,6 +12,11 @@ const CartReducer = (state = INITIAL_STATE, action ) => {
             return {
                 ...state,
                 hidden: !state.hidden
+            }
+        case cartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems,action.payload)
             }
         default:
             return state;
