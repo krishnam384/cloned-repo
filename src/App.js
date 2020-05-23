ba73c46b7a3783edca13c1af0ccde7cb0d0fe030
krishnam <saikrishnamraju384@gmail.com>
2020-05-23 12:17:47 +0530

    added selectors to stop unncecessary rerendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import { selectCurrentUser } from './redux/user/user.selector';
 import './App.css';
-
+import { createStructuredSelector } from 'reselect';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -61,8 +61,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.style.scss';
+import { createStructuredSelector } from 'reselect';
 
 const CartDropdown = ({cartItems}) => (
 
@@ -15,8 +17,8 @@ const CartDropdown = ({cartItems}) => (
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
 })
 
 export default connect(mapStateToProps)(CartDropdown);
